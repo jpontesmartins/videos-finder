f37e33d62b0bb5c502e71b3c304de99097b1fb4a
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-06-13 09:24:34 -0300

    test playlist organizer

---
+++ domain/PlaylistOrganizer/_tests/index.test.js
@@ -0,0 +1,24 @@
+const playlistOrganizer = require('../index');
+
+// - The user will input how much time he can expend daily during a week. For
+// example, [15, 120, 30, 150, 20, 40, 90] in minutes.
+
+// - The user will not expend more time watching videos than his daily max.
+
+// - The user will not start another video unless he can finish on the same day.
+
+// - Videos longer than the longest day will be ignored.
+
+// - The user will watch the videos in the exact order as returned.
+
+
+describe ('playlist organizer', () => {
+    it('should return 8 days to watch', () => {
+        const videosDuration = [20, 30, 60, 90, 200, 30, 40, 20, 60, 15]
+        const availableMinutes = [15, 120, 30, 150, 20, 40, 90];
+        
+        const totalOfDays = playlistOrganizer.calculateDaysToWatch(videosDuration, availableMinutes);
+        
+        expect(totalOfDays).toBe(8);
+    });
+});
